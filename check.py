083952ab06c8b7f271f51522d1638927f04bc136
Steve Lacy <me@slacy.me>
2018-06-21 13:27:38 -0400

    ensure it can run before tile38 is online

--- check.py
+++ check.py
@@ -8,12 +8,17 @@ url = "http://tile38-write:9851/server"
 not_ready = True
 
 while not_ready:
-    res = urllib.request.urlopen(url)
-    body = res.read().decode('utf-8')
-    obj = json.loads(body)
-    if obj['stats']['num_objects'] > 10:
-        not_ready = False
-        break
-    sys.stdout.write('.')
-    sys.stdout.flush()
-    time.sleep(1)
+    try:
+        res = urllib.request.urlopen(url)
+        body = res.read().decode('utf-8')
+        obj = json.loads(body)
+        if obj['stats']['num_objects'] > 10:
+            not_ready = False
+            break
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(1)
+    except:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+        time.sleep(1)
