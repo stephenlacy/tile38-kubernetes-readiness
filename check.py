5ef09a8eef4bfc63ef05a85820beb97ce16b49ef
Steve Lacy <me@slacy.me>
2018-06-21 13:37:07 -0400

    master and self urls

--- check.py
+++ check.py
@@ -2,23 +2,38 @@ import sys
 import urllib.request
 import json
 import time
-url = "http://tile38-write:9851/server"
-# url = "http://localhost:9851/server"
+url1 = "http://tile38-write:9851/server"
+url2 = "http://0.0.0.0:9851/server"
 
-not_ready = True
+master_not_ready = True
+self_not_ready = True
 
-while not_ready:
+def make_request(url):
+    res = urllib.request.urlopen(url)
+    body = res.read().decode('utf-8')
+    return json.loads(body)
+
+def wait():
+    sys.stdout.write('.')
+    sys.stdout.flush()
+    time.sleep(1)
+    return
+
+while master_not_ready:
     try:
-        res = urllib.request.urlopen(url)
-        body = res.read().decode('utf-8')
-        obj = json.loads(body)
+        obj = make_request(url1)
         if obj['stats']['num_objects'] > 10:
-            not_ready = False
+            master_not_ready = False
             break
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        time.sleep(1)
     except:
-        sys.stdout.write('.')
-        sys.stdout.flush()
-        time.sleep(1)
+        wait()
+
+while self_not_ready:
+    try:
+        obj = make_request(url2)
+        if obj['stats']['num_objects'] > 10:
+            self_not_ready = False
+            break
+    except:
+        wait()
+
